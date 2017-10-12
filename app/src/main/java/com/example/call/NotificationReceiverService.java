afb1e6236c6e0aff5638a09721c15926c83d6108
jasonRong <jasonrong@Martin-MBP.local>
2017-10-12 23:30:16 +0800

    first commit

---
+++ app/src/main/java/com/example/call/NotificationReceiverService.java
@@ -0,0 +1,20 @@
+package com.example.call;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.os.Build;
+import android.os.UserHandle;
+import android.service.notification.NotificationListenerService;
+import android.support.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+@SuppressLint("OverrideAbstract")
+public class NotificationReceiverService extends NotificationListenerService {
+    public NotificationReceiverService() {
+    }
+
+    @Override
+    public void onNotificationChannelModified(String pkg, UserHandle user, NotificationChannel channel, int modificationType) {
+        super.onNotificationChannelModified(pkg, user, channel, modificationType);
+    }
+}
